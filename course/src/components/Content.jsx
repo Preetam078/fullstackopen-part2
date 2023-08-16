3b3c1324af3da780d595a147abbe2f26e8a01d37
Preetam Mondal <preetammondal78@gmail.com>
2023-08-17 01:02:54 +0530

    done all the exercises till 2.5

--- course/src/components/Content.jsx
+++ course/src/components/Content.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import Part from './Part'
+import React from 'react';
+import Part from './Part';
+import Total from './Total';
 
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
+  const total = parts.reduce((accumulator, part) => accumulator + part.exercises, 0);
+
+  console.log(total);
+
   return (
     <div>
-        {
-            parts.map((part)=> (
-                <Part key={part.id} name={part.name} exercise={part.exercises}></Part>
-            ))
-        }
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercise={part.exercises}></Part>
+      ))}
+      <Total total={total}></Total>
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
